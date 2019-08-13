3899511645dddb790b617f8bd83c160abdc9e8e9
David Hardy <davehardy632@gmail.com>
2019-08-13 14:12:31 -0600

    started one to many relationships, completed lesson to write correctly formatted, synactictally sound tests.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var gamesRouter = require('./routes/api/v1/games');
-// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -15,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter)
 app.use('/api/v1/games', gamesRouter);
 
 module.exports = app;
--- seeders/20190808180638-game_seed.js
+++ seeders/20190808180638-game_seed.js
@@ -3,11 +3,34 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
+    return queryInterface.bulkInsert('Store', [{
+      id: 1,
+      name: 'Gamestop',
+      phoneNumber: 3032221111,
+    },
+    {
+      id: 2,
+      name: 'Games R Us',
+      phoneNumber: 3032331122,
+    },
+    {
+      id: 3,
+      name: 'Games and more Games',
+      phoneNumber: 3034443333,
+    },
+    {
+      id: 4,
+      name: 'We sell Games',
+      phoneNumber: 3038887777,
+    },
+    ], {});
+
     return queryInterface.bulkInsert('Games', [{
       title: 'Fix it Felix Jr.',
       price: 50,
       releaseYear: 1982,
       active: true,
+      StoreId: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -16,6 +39,7 @@ module.exports = {
       price: 100,
       releaseYear: 1981,
       active: true,
+      StoreId: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -24,6 +48,7 @@ module.exports = {
       price: 75,
       releaseYear: 1982,
       active: false,
+      StoreId: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
@@ -32,11 +57,13 @@ module.exports = {
       price: 125,
       releaseYear: 1981,
       active: true,
+      StoreId: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
     }
     ], {});
   },
+
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Games', null, {});
